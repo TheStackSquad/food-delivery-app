85011469e3401a3ad347007ba4383e3343997eb4
David Muriithi <muriithid05@gmail.com>
2024-11-19 22:55:32 +0300

    add image to database

--- backend/router/routes.js
+++ backend/router/routes.js
@@ -25,11 +25,24 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-router.post('/uploadProfilePicture', authMiddleware, upload.single('profilePicture'), (req, res) => {
+router.post('/uploadProfilePicture', authMiddleware, upload.single('profilePicture'), async (req, res) => {
     if (!req.file) {
-        return res.status(400).json({ message: 'No file uploaded' })
+        return res.status(400).json({ message: 'No file uploaded' });
     }
-    return res.status(200).json({ message: 'Profile pictire uploaded successfully', filePath: req.file.path });
+
+    try {
+        const user = await User.findById(req.user.userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        user.profilePicture = req.file.path;
+        await user.save();
+
+        res.status(200).json({ message: 'Profile picture uploaded successfully', filePath: req.file.path });
+    } catch (error) {
+        res.status(500).json({ message: 'Error saving profile picture', error });
+    }
 });
 
 // Protected route
